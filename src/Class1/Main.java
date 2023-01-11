ec186f0bcb3d5f8954b756c45fff34965f388525
nooriza <zafar.noori@gmail.com>
2023-01-11 13:15:17 -0500

    new commit

--- src/Class1/Main.java
+++ src/Class1/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am very computer follows what ever I say");
         System.out.println("I am very computer follows what ever I say");
-
+        System.out.println("Github was successful");
     }
 }
